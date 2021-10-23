dc59a3435e8635107c10b55636da1b884d279060
Sebastian Venancio <jsv.ven@outlook.com>
2021-10-23 10:51:57 -0300

    formulario con ajax_02

--- form.js
+++ form.js
@@ -1,19 +1,16 @@
 //Declaramos la url que vamos a usar para el GET
 const URLPOST   = "https://jsonplaceholder.typicode.com/posts"
 
-
-//Declaramos la información a enviar
-const infoPost =  { 
-    nombre: document.getElementById("nombre").value,
-    email: document.getElementById("email").value,
-    celu: document.getElementById("celu").value,
-    mensaje: document.getElementById("mensaje").value,
-  }
-
-  console.log(infoPost)
-
 //Boton enviar en el html buscado mediante el ID
-$("#contact-submit").click(() => { 
+$("#contact-submit").click(() => {
+    //Declaramos la información a enviar
+    const infoPost =  { 
+        nombre: document.getElementById("nombre").value,
+        email: document.getElementById("email").value,
+        celu: document.getElementById("celu").value,
+        mensaje: document.getElementById("mensaje").value,
+    }
+    
     $.post(URLPOST, infoPost ,(respuesta, estado) => {
         if(estado === "success"){
             $("body").append(`<div>
